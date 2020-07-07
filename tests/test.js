f9151d35024db0425ae42dd534bb4c43904e604d
dyu1208 <cindyhi128@gmail.com>
2020-07-07 12:04:20 +0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -10,6 +10,7 @@ describe('Tests app', function () {
   it('verifies get', function (done) {
     request.get('/').expect(200).end(function (err, result) {
       test.string(result.text).contains('CODING 让开发更简单');
+      test.string(result.text).contains('CODING 让开发更简单');
       test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
       done(err);
     });
